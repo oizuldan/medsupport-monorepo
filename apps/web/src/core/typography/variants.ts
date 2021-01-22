b2359b098d799d5d03dc63feceb2cd527a85286a
Dias177 <zhassanbay@gmail.com>
2021-01-22 19:21:49 +0600

    feat: add fonts

--- apps/web/src/core/typography/variants.ts
+++ apps/web/src/core/typography/variants.ts
@@ -1,33 +1,47 @@
 export enum Title {
-  Bold60 = 'ti_bold_60',
-  Regular60 = 'ti_regular_60',
   Bold34 = 'ti_bold_34',
-  Bold24 = 'ti_bold_24',
-  Bold20 = 'ti_bold_20',
-  Bold16 = 'ti_bold_16',
+  SemiBold34 = 'ti_semi_bold_34',
+  Medium34 = 'ti_medium_34',
+  Regular34 = 'ti_regular_34',
 }
 
 export enum Subtitle {
-  Bold24 = 'sub_bold_24',
-  Regular24 = 'sub_Regular_24',
+  Bold28 = 'sub_bold_28',
+  SemiBold28 = 'sub_semi_bold_28',
+  Medium28 = 'sub_medium_28',
+  Regular28 = 'sub_regular_28',
 }
 
 export enum Main {
-  Bold16 = 'main_bold_16',
-  Regular16 = 'main_regular_16',
+  Bold22 = 'main_bold_22',
+  SemiBold22 = 'sub_semi_bold_22',
+  Medium22 = 'sub_medium_22',
+  Regular22 = 'main_regular_22',
 }
 
 export enum Menu {
-  Bold14 = 'menu_bold_14',
+  Bold17 = 'menu_bold_17',
+  SemiBold17 = 'menu_semi_bold_17',
+  Medium17 = 'menu_medium_17',
+  Regular17 = 'menu_regular_17',
 }
 
-export enum Additional {
-  Regular12 = 'add_regular_12',
+export enum Content {
+  Regular24 = 'con_regular_24',
+  Regular20 = 'con_regular_20',
+  Regular16 = 'con_regular_16',
 }
 
-export enum Small {
-  Bold12 = 'sm_bold_12',
-  Regular12 = 'sm_regular_12',
+export enum Element {
+  Regular20 = 'el_regular_20',
+  SemiBold20 = 'el_semi_bold_20',
+  Bold20 = 'el_bold_20',
+  Regular16 = 'el_regular_16',
+  SemiBold16 = 'el_semi_bold_16',
+  Bold16 = 'el_bold_16',
+  Bold12 = 'el_bold_12',
+  SemiBold12 = 'el_semi_bold_12',
+  Regular12 = 'el_regular_12',
 }
 
-export type AnyTypography = Title | Subtitle | Main | Menu | Additional | Small;
+export type AnyTypography = Title | Subtitle | Main | Menu | Content | Element;
