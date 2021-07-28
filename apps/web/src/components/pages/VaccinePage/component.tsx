ccd7017e9364ba59d02d3bb8eb296ba8c787ffa1
zainoldin <zainoldinalmas@gmail.com>
2021-07-28 20:56:13 +0600

    feat(web): add intitial Vaccine Page

---
+++ apps/web/src/components/pages/VaccinePage/component.tsx
@@ -0,0 +1,11 @@
+import { Layout } from 'components';
+import { NextPage } from 'next';
+import React from 'react';
+
+export const VaccinePage: NextPage = () => {
+  return (
+    <Layout>
+      <div> Vaccine Page </div>
+    </Layout>
+  );
+};
